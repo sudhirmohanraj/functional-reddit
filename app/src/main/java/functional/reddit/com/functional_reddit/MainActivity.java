6d638769fc9bc6932a8a799f5e88d20ee59febba
Mohanraj, Sudhir <sudhir.mohanraj@capitalone.com>
2015-12-06 17:05:05 -0500

    Added a Button making an http request, but not retrieving any data yet.

---
+++ app/src/main/java/functional/reddit/com/functional_reddit/MainActivity.java
@@ -0,0 +1,79 @@
+package functional.reddit.com.functional_reddit;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+//import org.apache.http.client.HttpClient;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+    private static final String GET_URL = "http://google.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+        final Button button = (Button) findViewById(R.id.dummy_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendGET();
+            }
+        });
+    }
+
+    private void sendGET() {
+        try {
+            URL obj = new URL(GET_URL);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print result
+                System.out.println(response.toString());
+            } else {
+                System.out.println("GET request not worked");
+            }
+
+        } catch (Exception me) {
+            me.printStackTrace();
+        }
+    }
+
+}
